2fb26f8df6f26cdd850eb769ba53a379b55cf692
Talat Ataç <talatatac@gmail.com>
2018-06-02 16:19:29 +0300

    Splash Screen Örneği

---
+++ samples/SplashScreen2/app/src/main/java/com/example/talatatac/splashscreen/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.talatatac.splashscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch (InterruptedException e)
+                {
+
+                }
+            }
+        };
+        myThread.start();
+    }
+}
